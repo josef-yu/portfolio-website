d76785025c8a0ce6c937c5de9433cdd6142cea20
Joseph Yu <joseph@josefyu.com>
2026-05-24 14:50:30 +0800

    Redesign website to astro

---
+++ src/utilities/getTimeDiff.ts
@@ -0,0 +1,6 @@
+export function getTimeDiff(elapsed: number) {
+  const approxYears = elapsed / 31556952000 + Number.EPSILON;
+  const years = Math.round(approxYears * 10) / 10;
+
+  return years === 1 ? "1 year" : `${years} years`;
+}
