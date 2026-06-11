ac0c78e4846a1bdd4d539e9df47a1b7e8f59aebc
Joseph Yu <joseph@josefyu.com>
2026-06-11 23:43:37 +0800

    Added open graph image

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -5,6 +5,7 @@ import { contentAdmin } from './src/integrations/content-admin.ts';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://josefyu.com',
   integrations: [contentAdmin()],
   vite: {
     resolve: {
